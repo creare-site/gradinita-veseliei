8be87f97ae56deaa110b0aadc5ab11be38b189d6
App Generator <51070104+app-generator@users.noreply.github.com>
2024-03-22 10:39:38 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -105,16 +105,13 @@ const IndexPage = () => (
             <li className="icon fa-download"> 
               <a href="https://static.gradinita-veseliei.ro/mobilitate-personal-2024-2025.pdf" download>Mobilitatea Personalului Didactic 2024-2025</a>
             </li>
-            <li>
             <br />
 
             <li className="icon fa-download"> 
               <a href="https://static.gradinita-veseliei.ro/2021-09-stat-personal.pdf" download>Stat Personal pe niveluri de invatamant</a>
             </li>
-            <li>
             <br />
             
-            </li>
             <li className="icon fa-download"> 
               <a href="https://static.gradinita-veseliei.ro/transparenta-salariala.pdf" download>Transparenta Salariala (actualizat 2023-10-04)</a>
             </li>
